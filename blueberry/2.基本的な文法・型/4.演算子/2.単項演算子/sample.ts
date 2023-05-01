8abb96d718a9d39048a479e16490c411ff7286d7
Kohta9521 <kohta0421kochi@gmail.com>
2023-05-01 21:09:07 +0900

    add incriment decriment

---
+++ blueberry/2.基本的な文法・型/4.演算子/2.単項演算子/sample.ts
@@ -0,0 +1,20 @@
+// 単項演算子
+const x = 123;
+const minusx = -x;
+console.log(minusx);
+
+// 整数値へ変換
+const str: string = '123';
+console.log(+str + 100);
+
+// インクリメント デクリメント
+let foo = 10;
+foo ++;
+
+console.log(foo);
+--foo;
+console.log(foo);
+
+let too = 10;
+console.log(too++);
+console.log(too--);
