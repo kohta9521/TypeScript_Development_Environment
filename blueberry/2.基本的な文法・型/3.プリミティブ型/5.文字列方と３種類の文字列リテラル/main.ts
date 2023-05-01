e49b1034fc243d2732734fff29e500645394f6e8
Kohta9521 <kohta0421kochi@gmail.com>
2023-05-01 14:54:02 +0900

    add all fiels about escape chekens

---
+++ blueberry/2.基本的な文法・型/3.プリミティブ型/5.文字列方と３種類の文字列リテラル/main.ts
@@ -0,0 +1,14 @@
+// 文字列
+const str1: string = "Hello ";
+const str2: string = 'world!!';
+
+console.log(str1 + str2);
+
+// テンプレートリテラル
+const message: string = `Hello 
+world!!`;
+console.log(message);
+
+const str3: string = "Hello ";
+const str4: string = "world!!";
+console.log(`${str3}${str4}`);
