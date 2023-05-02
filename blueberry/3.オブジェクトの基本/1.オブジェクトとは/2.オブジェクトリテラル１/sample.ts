b9d2d26b09636c0b78fc61f55960a3d87f2bf69a
Kohta9521 <kohta0421kochi@gmail.com>
2023-05-03 07:28:44 +0900

    add 2023/05/03 first commit

--- blueberry/3.オブジェクトの基本/1.オブジェクトとは/2.オブジェクトリテラル１/sample.ts
+++ blueberry/3.オブジェクトの基本/1.オブジェクトとは/2.オブジェクトリテラル１/sample.ts
@@ -1,1 +1,18 @@
 // オブジェクトリテラル
+const sample = {
+  foo: 555,
+  bar: "文字列"
+};
+
+let input;
+
+const user = {
+  name: input ? input : "名無し",
+  age: 20
+}
+
+const name = input ? input : "名無し";
+const sample2 = {
+  name,
+  age: 20,
+};
