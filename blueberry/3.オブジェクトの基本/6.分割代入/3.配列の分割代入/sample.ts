8b412c1de9c8a4bd19ead3c3d21d8ea5170ac227
Kohta9521 <kohta0421kochi@gmail.com>
2023-05-05 16:30:46 +0900

    add 配列の分割代入

---
+++ blueberry/3.オブジェクトの基本/6.分割代入/3.配列の分割代入/sample.ts
@@ -0,0 +1,10 @@
+import { threadId } from "worker_threads";
+
+// 配列の分割代入
+const arr = [1, 2, 4, 8, 16, 32 ];
+
+const [first, second, thied] = arr;
+
+console.log(first);
+console.log(second);
+console.log(thied);
