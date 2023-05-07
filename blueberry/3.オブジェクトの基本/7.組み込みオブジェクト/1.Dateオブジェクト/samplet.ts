034ced147fea99f18089e3a76b5711bd7cc53cdc
Kohta9521 <kohta0421kochi@gmail.com>
2023-05-08 07:59:37 +0900

    add 2023/05/08 first commit

---
+++ blueberry/3.オブジェクトの基本/7.組み込みオブジェクト/1.Dateオブジェクト/samplet.ts
@@ -0,0 +1,10 @@
+// Dateオブジェクト
+const d = new Date();
+console.log(d); // 2021-01-01T00:00:00.000Z
+
+
+const a = ("2020-02-03T15:00:00+09:00");
+console.log(a);
+
+const date = new Date();
+const timeNum  = date.getTime();
