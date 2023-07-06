5fc40c70d903b7432d7871e87c2043927e16399b
Kohta9521 <kohta0421kochi@gmail.com>
2023-07-07 07:31:08 +0900

    add useState and useEffect ts lesson

--- usestate/src/App.tsx
+++ usestate/src/App.tsx
@@ -3,9 +3,30 @@ import { Counter } from './features/counter/Counter';
 import './App.css';
 
 const App: React.FC = () => {
+  const [ status, setStatus ] = useState<string | number>("text");
+  const [ input, setInput ] = useState<string>("")
+  const [ counter, setCounter ] = useState(0);
+
+  const onChangeHandler = (e:  React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  }
+  useEffect(() => {
+    console.log('useEffect in app component invoked !');
+    document.title = `current value is ${counter}`;
+  })
+
   return (
     <div className="App">
       <header className="App-header">
+        <h4>{status}</h4>
+        <button onClick={() => setStatus("new Text!")}>Button</button>
+
+        <h4>{input}</h4>
+        <input type="text" value={input} onChange={onChangeHandler} />
+
+        <h4>{counter}</h4>
+        <button onClick={() => setCounter((preCounter) => preCounter + 1)}>Increment</button>
+
         <Counter />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
