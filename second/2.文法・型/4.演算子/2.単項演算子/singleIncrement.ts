4b35c176e12747d7b8f77a62c3f87980b6ef89b5
Kohta9521 <kohta0421kochi@gmail.com>
2023-07-10 16:55:17 +0900

    add new section

---
+++ second/2.文法・型/4.演算子/2.単項演算子/singleIncrement.ts
@@ -0,0 +1,15 @@
+export{}
+
+const x = 123;
+const minusx: number = -x;
+console.log(minusx);
+
+const str: string = '123';
+console.log(+str * 100);
+
+let foo  = 10;
+foo ++;
+console.log(foo);
+
+--foo;
+console.log(foo);
---
+++ second/2.文法・型/4.演算子/3.文字列の結合/stringPlus.ts
@@ -0,0 +1,8 @@
+export{}
+import { createInterface } from "readline";
+
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
