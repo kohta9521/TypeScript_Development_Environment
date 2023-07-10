f60d345df8592227d25f26c8109cca1d79575364
Kohta9521 <kohta0421kochi@gmail.com>
2023-07-10 17:25:24 +0900

    add new vite project storybook ts react project

--- storybook/src/stories/HelloButton.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { ComponentMeta } from "@storybook/react";
-import { Button } from "./Button";
-
-export default {
-  title: 'Button',
-  component: Button,
-} as ComponentMeta<typeof Button>;
-
-export const HelloButton = () => <Button label="Hello!!" />;
