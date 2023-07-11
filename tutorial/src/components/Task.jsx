40e87721f67b3ba8c245e674061051a47d1eb370
Kohta9521 <kohta0421kochi@gmail.com>
2023-07-11 20:12:49 +0900

    add 2023/07/11 first commit

--- tutorial/src/components/Task.jsx
+++ tutorial/src/components/Task.jsx
@@ -1,1 +1,11 @@
 import React from "react";
+
+export default function Task({ task: { di, title, state }, onArchiveTask, onPinTask }) {
+    return (
+        <div className="list-item">
+            <label htmlFor="title" aria-label={title}>
+                <input type="text" value={title} readOnly={true} name="title" />
+            </label>
+        </div>
+    )
+}
