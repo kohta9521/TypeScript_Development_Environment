aee48ce0af0bf817e9e816e87399907947c7b09a
Kohta9521 <kohta0421kochi@gmail.com>
2023-07-18 07:20:08 +0900

    add restudy about useStte and useEffect

--- redux/src/App.tsx
+++ redux/src/App.tsx
@@ -4,12 +4,30 @@ import './App.css';
 
 const App: React.FC = () => {
 
-  const [ status, setStatus ] = useState<string>("text");
+  const [ status, setStatus ] = useState<string | number>("text");
+
+  const [ input, setInput ] = useState<string>("");
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  }
+
+  const [ counter, setCounter ] = useState(0);
+
+  useEffect(() => {
+    console.log("useEffect in App component invoked !");
+    document.title = `current value is ${counter}`
+  }, [counter])
 
   return (
     <div className="App">
       <header className="App-header">
         <h4>{status}</h4>
+        <button onClick={() => setStatus(1)}>Button</button>
+        <h4>{input}</h4>
+        <input type="text" value={input} onChange={onChangeHandler} />
+
+        <h4>{counter}</h4>
+        <button onClick={() => setCounter((preCounter) => preCounter + 1)}>Increment</button>
         <Counter />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
